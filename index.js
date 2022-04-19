576648665b5b2d94368ef4b40a0c70f26e5b1f91
skj99667788@gmail.com <skj99667788@gmail.com>
2022-04-19 05:27:34 +0500

    Navbar working with JS

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-function displayList(){
-    const Addbook = document.getElementById('Add-Books');
-    // const displalist = document.getElementById('Book-List');
-    if(  Addbook.style.display === 'flex'){
-    Addbook.style.display = 'none';}
-    // displalist.style.display = 'none';
-    else{
-        Addbook.style.display = 'flex';
-    }
+var Addbook = document.getElementById('Add-Books');
+var displalist = document.getElementById('Book-List');
+var displacontact = document.getElementById('contact');
+function displayAdd() {
+  if (Addbook.style.display === 'none') {
+    Addbook.style.display = 'flex';
+    displalist.style.display = 'none';
+    displacontact.style.display = 'none';
+  } else {
+    Addbook.style.display = 'flex';
+    displalist.style.display = 'none';
+    displacontact.style.display = 'none';
+  }
+}
+function displayList() {
+    if (displalist.style.display === 'none') {
+        Addbook.style.display = 'none';
+        displalist.style.display = 'flex';
+        displacontact.style.display = 'none';
+      } else {
+        Addbook.style.display = 'none';
+        displalist.style.display = 'flex';
+        displacontact.style.display = 'none';
+      }
+}
+
+function displayContact() {
+    if ( displacontact.style.display === 'none') {
+        Addbook.style.display = 'none';
+        displalist.style.display = 'none';
+        displacontact.style.display = 'flex';
+      } else {
+        Addbook.style.display = 'none';
+        displalist.style.display = 'none';
+        displacontact.style.display = 'flex';
+      }
 }
 
 function onFormSubmit() {
@@ -16,29 +43,26 @@ function onFormSubmit() {
 
 function readFormData() {
   var formData = {};
-  formData["author"] = document.getElementById("author").value;
-  formData["book"] = document.getElementById("book").value;
+  formData['author'] = document.getElementById('author').value;
+  formData['book'] = document.getElementById('book').value;
   return formData;
 }
 
-
 function insertNewRecord(data) {
-  var table = document.getElementById("List").getElementsByTagName("tbody")[0];
+  var table = document.getElementById('List').getElementsByTagName('tbody')[0];
   var newRow = table.insertRow(table.length);
   cell1 = newRow.insertCell(0);
   cell1.innerHTML = data.author;
   cell2 = newRow.insertCell(1);
   cell2.innerHTML = data.book;
   cell3 = newRow.insertCell(2);
-  cell3.innerHTML = `<button  onClick="onDelete(this)">Delete</button>`;
+  cell3.innerHTML = `<button  onClick='onDelete(this)'>Delete</button>`;
 }
 
 function onDelete(td) {
-  if (confirm("Are you sure to delete this record?")) {
+  if (confirm('Are you sure to delete this record?')) {
     row = td.parentElement.parentElement;
-    document.getElementById("List").deleteRow(row.rowIndex);
+    document.getElementById('List').deleteRow(row.rowIndex);
     resetForm();
   }
 }
-
-
