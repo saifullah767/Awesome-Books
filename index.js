c0793672d8841081772518635fd2288c2966747c
skj99667788@gmail.com <skj99667788@gmail.com>
2022-04-19 15:42:37 +0500

    Display book js set

--- index.js
+++ index.js
@@ -30,3 +30,39 @@ const getStorageData = () => {
     awesomeBooks = localFormData;
   }
 };
+
+window.onload = getStorageData();
+
+// Display Books Data
+
+const displayBooks = () => {
+  const booksList = document.querySelector('.books');
+  booksList.innerHTML = '';
+  awesomeBooks.forEach((book) => {
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+
+    const headTitle = document.createElement('h2');
+    headTitle.classList.add('title');
+    headTitle.textContent = book.title;
+
+    const headAuthor = document.createElement('h2');
+    headAuthor.classList.add('author');
+    headAuthor.textContent = book.author;
+
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+
+    removeBtn.addEventListener('click', () => {
+      removeBook(book.id);
+      displayBooks();
+    });
+    bookElement.appendChild(headTitle);
+    bookElement.appendChild(headAuthor);
+    bookElement.appendChild(removeBtn);
+    booksList.appendChild(bookElement);
+    const hr = document.createElement('hr');
+    booksList.appendChild(hr);
+  });
+  saveBooks();
+};
