2a7226aceb10b44115e9ac0bca8a705cb84b1661
skj99667788@gmail.com <skj99667788@gmail.com>
2022-04-19 14:13:06 +0500

    Functions restore

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 /* global row, cell13*/
+var displaList = document.getElementById('Book-List');
+var displaBooks = document.getElementById('Add-Books');
+var displaContact = document.getElementById('contact');
+
+function displayList(){
+  if(displaList.style.display === 'none'){
+    displaList.style.display = 'flex'
+    displaBooks.style.display = 'none'
+    displaContact.style.display = 'none'
+  } else{
+    displaList.style.display = 'flex'
+    displaBooks.style.display = 'none'
+    displaContact.style.display = 'none'
+  }
+  
+}
+
+function displayAdd(){
+  if(displaBooks.style.display === 'none'){
+    displaList.style.display = 'none'
+    displaBooks.style.display = 'flex'
+    displaContact.style.display = 'none'
+  } else{
+    displaList.style.display = 'none'
+    displaBooks.style.display = 'flex'
+    displaContact.style.display = 'none'
+  }
+
+}
+
+function displayContact(){
+  if(displaContact.style.display === 'none'){
+    displaList.style.display = 'none'
+    displaBooks.style.display = 'none'
+    displaContact.style.display = 'flex'
+  } else{
+    displaList.style.display = 'none'
+    displaBooks.style.display = 'none'
+    displaContact.style.display = 'flex'
+  }
+
+}
+
 function onFormSubmit() {
   var formData = readFormData();
   insertNewRecord(formData);
 }
-
 function readFormData() {
   var formData = {};
   formData['author'] = document.getElementById('author').value;
