96a76c0778e72c75f309230920c1821ade6da009
skj99667788@gmail.com <skj99667788@gmail.com>
2022-04-20 20:31:16 +0500

    Function add for navigation

--- script.js
+++ script.js
@@ -103,3 +103,49 @@ window.onbeforeunload = () => {
   getStorageData();
   displayBooks();
 };
+
+// Navigation
+
+var displaList = document.getElementById('Book-List');
+var displaBooks = document.getElementById('Add-Books');
+var displaContact = document.getElementById('contact');
+
+function displayList(){
+  if(displaList.style.display === 'none'){
+    displaList.style.display = 'flex'
+    displaBooks.style.display = 'none'
+    displaContact.style.display = 'none'
+  } else{
+    displaList.style.display = 'flex'
+    displaBooks.style.display = 'none'
+    displaContact.style.display = 'none'
+  }
+  
+}
+
+function displayAdd(){
+  if(displaBooks.style.display === 'none'){
+    displaList.style.display = 'none'
+    displaBooks.style.display = 'flex'
+    displaContact.style.display = 'none'
+  } else{
+    displaList.style.display = 'none'
+    displaBooks.style.display = 'flex'
+    displaContact.style.display = 'none'
+  }
+
+}
+
+function displayContact(){
+  if(displaContact.style.display === 'none'){
+    displaList.style.display = 'none'
+    displaBooks.style.display = 'none'
+    displaContact.style.display = 'flex'
+  } else{
+    displaList.style.display = 'none'
+    displaBooks.style.display = 'none'
+    displaContact.style.display = 'flex'
+  }
+
+}
+
